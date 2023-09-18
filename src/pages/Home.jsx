83292da8df1e19451e84b4b5cee6b84d971e5233
Igor Postnikov <igor.postnikov8@gmail.com>
2023-09-18 12:13:03 -0700

    Added kanban project into projects page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Home = () => {
+
+    return (
+        <div style={{ alignSelf: "stretch" }}>
+            <h1 style={{ textAlign: 'center' }}>Hi 👋, I'm Igor Postnikov</h1>
+            <h3 style={{ textAlign: 'center' }}>A passionate frontend developer from Mountain View, California</h3>
+            <br/>
+            <div style={{ paddingRight: '360px' }}>
+                <img
+                    style={{ float: 'right' }}
+                    className="avatar rounded-2 avatar-user"
+                    src="https://avatars.githubusercontent.com/u/26352039?s=400&u=ff47db5725a8017a979d1cf72aeb5832525f00dc&v=4"
+                    width="400"
+                    height="400"
+                    alt="@igorpsf"
+                />
+            </div>
+
+            <ul style={{ paddingLeft: '360px' }}>
+                <li>👨‍💻 All of my projects are available at <a href="https://igorp.io">igorp.io</a></li>
+                <li>📫 How to reach me <a href="mailto:igor.postnikov8@gmail.com">igor.postnikov8@gmail.com</a></li>
+                <li><img src="https://icons.iconarchive.com/icons/limav/flat-gradient-social/128/Linkedin-icon.png" width="22" height="22"></img> Connect with me: <a href="https://www.linkedin.com/in/igorpostnikov/">www.linkedin.com/in/igorpostnikov</a></li>
+            </ul>
+
+        </div>
+    );
+};
+
+export default Home;
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,10 +1,77 @@
 import React from 'react';
+import {Grid, Typography, Paper, Button, Chip } from "@mui/material";
+// import ReactIcon from './icons/react.svg';
+// import JavaScriptIcon from './icons/react.svg';
+// import AWSIcon from './icons/react.svg';
+// import JenkinsIcon from './icons/react.svg';
+
 
 const Projects = () => {
+    const projects = [
+        {
+            title: 'Kanban',
+            description: 'Kanban  + Kanban Server',
+            imageUrl: 'https://igorp-io-images.s3.us-west-1.amazonaws.com/Screenshot+2023-09-18+at+12.08.51%E2%80%AFPM.png',
+            projectLink: 'https://kanban.igorp.io',
+            githubLink: 'https://github.com/igorpsf/kanban',
+            technologies: [
+                { name: 'React' },
+                { name: 'JavaScript' },
+                { name: 'AWS' },
+                { name: 'Jenkins' },
+            ]
+        },
+        // {
+        //     title: 'Project 2',
+        //     description: 'Description of Project 2',
+        //     imageUrl: 'https://via.placeholder.com/200',
+        //
+        // },
+        // {
+        //     title: 'Project 3',
+        //     description: 'Description of Project 3',
+        //     imageUrl: 'https://via.placeholder.com/200',
+        //
+        // },
+    ];
     return (
         <div>
-            <h1>Projects</h1>
-
+            <Grid container spacing={3} style={{ padding: '20px' }}>
+                {projects.map((project, index) => (
+                    <Grid item xs={12} sm={6} md={4} key={index}>
+                        <Paper elevation={3} style={{ padding: '20px' }}>
+                            <img
+                                src={project.imageUrl}
+                                alt={project.title}
+                                style={{ width: '100%' }}
+                            />
+                            <Typography variant="h6" style={{ marginTop: '10px' }}>
+                                {project.title}
+                            </Typography>
+                            <Typography variant="body2" color="textSecondary">
+                                {project.description}
+                            </Typography>
+                            <div style={{ marginTop: '10px' }}>
+                                <Button
+                                    variant="outlined"
+                                    color="primary"
+                                    style={{ marginRight: '10px' }}
+                                    onClick={() => window.location.href = project.projectLink}
+                                >
+                                    Project URL
+                                </Button>
+                                <Button
+                                    variant="outlined"
+                                    color="secondary"
+                                    onClick={() => window.location.href = project.githubLink}
+                                >
+                                    GitHub
+                                </Button>
+                            </div>
+                        </Paper>
+                    </Grid>
+                ))}
+            </Grid>
         </div>
     );
 };
