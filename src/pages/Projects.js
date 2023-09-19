a369d552d9099c6fc57b1cebbdf2932fb636b285
Igor Postnikov <igor.postnikov8@gmail.com>
2023-09-19 11:28:35 -0700

    Modified Kanban project links and description

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import {Grid, Typography, Paper, Button } from "@mui/material";
-// import ReactIcon from './icons/react.svg';
-// import JavaScriptIcon from './icons/react.svg';
-// import AWSIcon from './icons/react.svg';
-// import JenkinsIcon from './icons/react.svg';
 
 
 const Projects = () => {
+
     const projects = [
         {
             title: 'Kanban',
             description: 'Kanban  + Kanban Server',
             imageUrl: 'https://igorp-io-images.s3.us-west-1.amazonaws.com/Screenshot+2023-09-18+at+12.08.51%E2%80%AFPM.png',
-            projectLink: 'kanban.igorp.io',
+            projectLink: 'http://kanban.igorp.io',
             githubLink: 'https://github.com/igorpsf/kanban',
             technologies: [
                 { name: 'React' },
                 { name: 'JavaScript' },
-                { name: 'AWS' },
-                { name: 'Jenkins' },
+                { name: 'Material UI' },
+                { name: '(AWS) - Route53, S3, EC2' },
             ]
         },
         // {
@@ -39,7 +36,7 @@ const Projects = () => {
             <Grid container spacing={3} style={{ padding: '20px' }}>
                 {projects.map((project, index) => (
                     <Grid item xs={12} sm={6} md={4} key={index}>
-                        <Paper elevation={3} style={{ padding: '20px' }}>
+                        <Paper elevation={3} style={{ padding: '20px'}}>
                             <img
                                 src={project.imageUrl}
                                 alt={project.title}
@@ -51,7 +48,7 @@ const Projects = () => {
                             <Typography variant="body2" color="textSecondary">
                                 {project.description}
                             </Typography>
-                            <div style={{ marginTop: '10px' }}>
+                            <div style={{ marginTop: '10px'}}>
                                 <Button
                                     variant="outlined"
                                     color="primary"
@@ -67,6 +64,16 @@ const Projects = () => {
                                 >
                                     GitHub
                                 </Button>
+                                <div style={{ marginTop: '10px', height: '200px', overflowY: 'scroll' }}>
+                                    <Typography variant="body2" color="textSecondary">
+                                        Technologies:
+                                    </Typography>
+                                    <ul>
+                                        {project.technologies.map((tech, techIndex) => (
+                                            <li key={techIndex}>{tech.name}</li>
+                                        ))}
+                                    </ul>
+                                </div>
                             </div>
                         </Paper>
                     </Grid>
